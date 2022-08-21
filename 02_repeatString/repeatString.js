21a712b383356b7be794ffaa5deccbe50dd9aef3
Paarth <paarthguardian@gmail.com>
2022-08-21 10:53:14 -0500

    Complete Exercise and Unskip tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let answer = '';
+    for (let i  = 0; i < times; i++) {
+        answer += word;
+    }
+    if (times < 0) {
+        answer = 'ERROR';
+    }
+    return answer;
 };
 
 // Do not edit below this line
