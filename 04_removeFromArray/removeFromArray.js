3cce91d855647a70d123f472b8085fb45221d856
Paarth <paarthguardian@gmail.com>
2022-08-23 14:04:20 -0500

    Complete Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayz, ...remove) {
+    for (let item of remove) {
+        for (let i = 0; i < arrayz.length; ++i) {
+            if (arrayz[i] === item) arrayz.splice(i,1);
+        }
+    }
+    return arrayz;
 };
 
 // Do not edit below this line
