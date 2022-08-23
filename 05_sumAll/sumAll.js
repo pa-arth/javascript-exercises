986bcc167630ef68b3dbca7673e0543cd596256a
Paarth <paarthguardian@gmail.com>
2022-08-23 14:19:39 -0500

    Complete exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let ans = 0;
+    if (typeof int1 == "number" && typeof int2 ==  "number" && int1 >= 0 && int2 >= 0) {
+        for (let i = Math.min(int1, int2); i <= Math.max (int1, int2); i++) {
+            ans += i;
+        }
+    }
+    else {
+        ans = 'ERROR'
+    }
+    return ans;
 };
 
 // Do not edit below this line
