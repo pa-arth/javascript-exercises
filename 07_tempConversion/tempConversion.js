91aeee8baf6aac3ba09f680b74bfb12bf2153f5b
Paarth <paarthguardian@gmail.com>
2022-08-23 14:33:59 -0500

    Finish Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let ans; 
+  ans = (temp -32 )* 5/9;
+  
+  ans = Math.round(ans * 10) / 10;
+  return ans;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let ans; 
+  ans = (temp) * 9/5 + 32;
+  ans = Math.round(ans * 10) / 10;
+  return ans;
 };
 
 // Do not edit below this line
